df6962d004ba7a9c7582f5b35029c81cce19311d
4rjunc <arjunmundamani@gmail.com>
2024-09-29 16:17:25 +0530

    cNFT : 🚨

--- README.md
+++ README.md
@@ -12,4 +12,15 @@ To run:
 bun run index.ts
 ```
 
-This project was created using `bun init` in bun v1.1.20. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+Blogs Refered
+
+1. The most detailed blog on Solana's compression: https://www.helius.dev/blog/all-you-need-to-know-about-compression-on-solana
+2. All about specifically NFT compression (cNFTS): https://www.helius.dev/blog/solana-nft-compression
+3. Metaplex's Bubblegum program's docs: https://developers.metaplex.com/bubblegum
+4. Merkle Tress: https://www.helius.dev/blog/cryptographic-tools-101-hash-functions-and-merkle-trees-explained#what’s-a-merkle-tree
+
+5. Creating compressed NFTs using Javascript: https://solana.com/developers/guides/javascript/compressed-nfts
+6. The most detailed blog on Solana's compression: https://www.helius.dev/blog/all-you-need-to-know-about-compression-on-solana
+7. All about specifically NFT compression (cNFTS): https://www.helius.dev/blog/solana-nft-compression
+8. Metaplex's Bubblegum program's docs: https://developers.metaplex.com/bubblegum  
+   This project was created using `bun init` in bun v1.1.20. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- index.ts
+++ index.ts
@@ -25,15 +25,48 @@ import {
   verifyCollection,
   mintToCollectionV1,
 } from "@metaplex-foundation/mpl-bubblegum";
-import { symbolName } from "typescript";
 
-//const endpoint = "https://api.devnet.solana.com";
-const endpoint = "https://api.mainnet-beta.solana.com";
+const endpoint = "https://api.devnet.solana.com";
+//const endpoint = "https://api.mainnet-beta.solana.com";
 
 const mintCNFT = async () => {
   const fellowAddresses = [
-    "7yxySxVMxgwMJ3TMBboPfAqZmkDvuMzBLQQ39GeFQUks",
-    //"6X4G9p5kiE6tDXkBHfpqispJ2B6YfAA3tBGcKvaXaht2",
+    "BSxNuBrnCnpEcNGpnt3Hb6iqF3vfh33hfmLKREtgezSa", //nju address
+    "EbN3JqZ2EKGGVagmvQpUuUPopEMDaXq4yXbLNqhaXEZD", //my second acc.
+    "7jQFJLS3QRGJyshYkLgp4QQH8D5c9qym2LQzkhag38UD",
+    "8J9Hz2tfFLDhE5vcdbinCMug4xqyBCfQCoi4QYfVapEn",
+    "A1mq3dn2tUBfJB6WjnL4XtVQgGLGAUD3FeiMLuUQoRMu",
+    "HjJQdfTHgC3EBX3471w4st8BXbBmtbaMyCAXNgcUb7dq",
+    "BtSTqq27A7xTMaCPWEhNwdf4eHsLWiWZvhQS2ABMd1Y4",
+    "9riZWGcTFTLoBpmRM5xfYXCrHsxoqL4ynqBYtNxskYHV",
+    "H3QFot1G5Xe8wAjkQbLLt5dEYsHBsicKLHL1aSBv2H2d",
+    "G1ZRP9Sz87SZJ6ZdsqaK8QxbXGTwCFv1SYnheRtY63DW",
+    "8MgdhXTpfWp5k2m1Q2CxMkETgenkYasNqGW88nUANRkR",
+    "6X4G9p5kiE6tDXkBHfpqispJ2B6YfAA3tBGcKvaXaht2",
+    "8HWXSHAngoGE9dudeZUcvnP7xRr9Wb4gy7H8VS5GRo7N",
+    "9BbWp6tcX9MEGSUEpNXfspYxYsWCxE9FgRkAc3RpftkT",
+    "3dfxtPdadK4CdHC1HjcD6Fc2J3x3REy55RyDxAfYuf1d",
+    "Fhrr8gFyNAASoCM2GprrjaNahgCJyb5SVV6V5oHr72Fj",
+    "DVxLfD4BFF3dLUtpvDZ5jbZExjknE1m2WwH2axz2J6ge",
+    "3o5cfcL9VS31T9N5ZbQgLTHokpxiWbTtjoAMjUp2SNey",
+    "9unenHYtwUowNkWdZmSYTwzGxxdzKVJh7npk6W6uqRF3",
+    "3dTSLCGStegkuoU6dc75DbRdJk4rKV3d5ZCZdSWbTcQv",
+    "6ggGtCSpE6moyjDhQQ7MfQ8cw89DcgtYJhaKZaKJ59CQ",
+    "JCsFjtj6tem9Dv83Ks4HxsL7p8GhdLtokveqW7uWjGyi",
+    "DH9oe9rfZWkRfBVWvib11ihrgCaYP1jGrD9fXcvhun37",
+    "HdaKENyK8fxod85QipFYZffC82PmsM8XEW4prcZbeQiK",
+    "EcrHvqa5Vh4NhR3bitRZVrdcUGr1Z3o6bXHz7xgBU2FB",
+    "GyETGp22PjuTTiQJQ2P9oAe7oioFjJ7tbTBr1qiXZoa8",
+    "frae7AtwagcebTnNNFaobGH2haFUGNpFniKELbuBi2z",
+    "38rc27bLd73QUDKmiDBQjsmbXpxinx8metaPFsRPSCWi",
+    "4syk2oXfU7kgpAPAxsyBv47FHeNuVz5WGc2x8atGNDd2",
+    "HFJEhqTUPKKWvhwVeQS5qjSP373kMUFpNuiqMMyXZ2Gr",
+    "72hBoHW3TDBHH8vASheaqwVAb8ez3SJAhwtegN5UQvJ9",
+    "CxjawXnJxAyb7Zx3xCkSD3nxamdpcfSikvnnC7C8RMHh",
+    "2hNdA3G3hfwUN6z28mgFTAjmkXdTvHsRiTXQP3AZjaij",
+    "ji1E9W3P4Yesmwcv6m5rgBs6dGnshaTcfaFoRW6qcjL",
+    "HT8DNntQe2ZN1v763zUqPou5wwNGTg6xBPCDg31vhjrv",
+    "BsdgGRzDmVTM8FBepRXrQixMZgjP6smsSbuDb1Y7VJB6",
   ];
 
   //SETTING UP UMI
@@ -41,7 +74,7 @@ const mintCNFT = async () => {
     .use(mplBubblegum())
     .use(mplTokenMetadata())
     .use(irysUploader());
-  console.log("🛠 UMI: ", umi);
+  // console.log("🛠 UMI: ", umi);
 
   // GENERATE NEW KEYPAIR SIGNER
   const signer = generateSigner(umi);
@@ -50,7 +83,7 @@ const mintCNFT = async () => {
   const wallet = await Bun.file("my-keypair.json").text();
   //const secretKey = new Uint8Array(JSON.parse(wallet));
   const secretKey = new Uint8Array(JSON.parse(wallet));
-  console.log(`☘️  Secret Key: ${secretKey}`);
+  //console.log(`☘️  Secret Key: ${secretKey}`);
   let keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
 
   // LOAD KEYPAIR INTO UMI
@@ -61,40 +94,21 @@ const mintCNFT = async () => {
   // CREATE A MERKLE TREE
   const createTreeTx = await createTree(umi, {
     merkleTree,
-    maxDepth: 14,
-    maxBufferSize: 64,
-    canopyDepth: 0,
+    maxDepth: 5,
+    maxBufferSize: 8,
+    canopyDepth: 2,
   });
 
   await createTreeTx.sendAndConfirm(umi);
 
   // CREATE TOKEN METADATA
   const collectionID = generateSigner(umi);
-  const NFTImageURI = [
-    "https://pbs.twimg.com/profile_images/1837114156777889793/UZAq8VY4_400x400.jpg",
-  ];
-  console.log("collectionID", collectionID);
+  console.log("⭐️ COLLECTION ID", collectionID);
 
-  const collectionMetadata = {
-    name: "arjun",
-    symbol: "AC",
-    description: "zero to something in solana",
-    image: NFTImageURI[0],
-    externalUrl: "https://x.com/4rjunc",
-    properties: {
-      files: [
-        {
-          uri: NFTImageURI[0],
-          type: "image/png",
-        },
-      ],
-    },
-  };
-
-  console.log("UPLOADING COLLECTION METADATA");
-  const collectionMetadataUri = await umi.uploader.uploadJson(
-    "https://github.com/4rjunc/cNFT/blob/main/collectionMetadata.json",
-  );
+  //console.log("UPLOADING COLLECTION METADATA");
+  //const collectionMetadataUri = await umi.uploader.uploadJson(collectionMetadata);
+  const collectionMetadataUri =
+    "https://jbtvvzbsryxfzsfvkyia.supabase.co/storage/v1/object/public/solanamob/collectionMetadata.json";
   console.log("CREATING COLLECTION NFT");
   await createNft(umi, {
     mint: collectionID,
@@ -104,23 +118,10 @@ const mintCNFT = async () => {
     sellerFeeBasisPoints: percentAmount(0),
   }).sendAndConfirm(umi);
 
-  const nftMetadata = {
-    name: "4rjunc",
-    image: NFTImageURI[0],
-    externalUrl: "https://x.com/4rjunc",
-    attributes: [
-      { trait_type: "twitter", value: "https://twitter.com/4rjunc" },
-      { trait_type: "github", value: "https://github.com/4rjunc" },
-    ],
-    properties: {
-      files: [{ uri: NFTImageURI[0], type: "image/png" }],
-    },
-  };
-
-  console.log("UPLOADING CNFT METADATA");
-  const nftMetadataUri = await umi.uploader.uploadJson(nftMetadata);
-
-  console.log("merkleTree:", merkleTree);
+  //console.log("UPLOADING CNFT METADATA");
+  //  const nftMetadataUri = await umi.uploader.uploadJson(nftMetadata);
+  const nftMetadataUri =
+    "https://jbtvvzbsryxfzsfvkyia.supabase.co/storage/v1/object/public/solanamob/cNFTMetadata.json";
   for (const student of fellowAddresses) {
     console.log("Student Address:", student);
     const newOwner = publicKey(student);
@@ -134,7 +135,7 @@ const mintCNFT = async () => {
       collectionMint: collectionID.publicKey,
       metadata: {
         name: "4rjunc",
-        uri: "https://github.com/4rjunc/cNFT/blob/main/cNFTMetadata.json", // Either use `nftMetadataUri` or a previously uploaded uri.""
+        uri: nftMetadataUri, // Either use `nftMetadataUri` or a previously uploaded uri.""
         sellerFeeBasisPoints: 500, // 5%
         collection: { key: collectionID.publicKey, verified: false },
         creators: [
